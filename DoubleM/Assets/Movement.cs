6ceeb3a5200c405f3a18beee25edc0b9774095b1
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2020-02-27 21:29:46 +0200

    Main character graphics, movement, joystick
    https://doublemktu.atlassian.net/jira/software/projects/DOUB/boards/1?selectedIssue=DOUB-8 DOUB-8
    
    https://doublemktu.atlassian.net/jira/software/projects/DOUB/boards/1?selectedIssue=DOUB-71 DOUB-71

---
+++ DoubleM/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Joystick joystick;
+    public Rigidbody2D rb;
+    public float speed;
+    Vector2 movement;
+    
+    void Update()
+    {
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
