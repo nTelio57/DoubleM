2232afbecc909aa007be96299fd52aba9cafa7a7
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2020-03-05 21:34:49 +0200

    Main hero movement/idle animations
    https://doublemktu.atlassian.net/browse/DOUB-86 DOUB-86

--- DoubleM/Assets/Scripts/Movement.cs
+++ DoubleM/Assets/Scripts/Movement.cs
@@ -6,13 +6,21 @@ public class Movement : MonoBehaviour
 {
     public Joystick joystick;
     public Rigidbody2D rb;
+    public Animator animator;
     public float speed;
     Vector2 movement;
+    float memoryMovementX;
     
     void Update()
     {
         movement.x = joystick.Horizontal;
         movement.y = joystick.Vertical;
+
+        if (movement.x != 0)
+            memoryMovementX = movement.x;
+
+        animator.SetFloat("Speed", joystick.Direction.sqrMagnitude);
+        animator.SetFloat("Horizontal", memoryMovementX);
     }
 
     private void FixedUpdate()
