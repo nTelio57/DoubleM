532e6d4a9a98087c089f02648bdf307b9ffaec51
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2020-04-01 15:18:43 +0300

    Player running mechanics with stamina and running animation added, stage 3 basics added
    https://doublemktu.atlassian.net/browse/DOUB-111

--- DoubleM/Assets/Scripts/Movement.cs
+++ DoubleM/Assets/Scripts/Movement.cs
@@ -8,28 +8,89 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public float speed;
+    public float runSpeed;
+    public float maxStamina;
+    public float staminaGainedOnRest;
+    public float staminaLostOnRun;
     Vector2 movement;
     float memoryMovementX;
+    bool isRunning;
+    float stamina;
+    bool resetNeeded;
+    float timer = 0;
 
     void Start()
     {
         joystick = JoystickManager.currentJoystick;
+        stamina = maxStamina;
+        resetNeeded = false;
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
         movement.x = JoystickManager.currentJoystick.Horizontal;
         movement.y = JoystickManager.currentJoystick.Vertical;
 
         if (movement.x != 0)
+        {
             memoryMovementX = movement.x;
+        }
+        else
+        {
+            resetNeeded = false;
+        }
+
+        if (JoystickManager.currentJoystick.Direction.sqrMagnitude >= 0.95 && stamina > 0 && !resetNeeded)
+        {
+            isRunning = true;
+            animator.SetBool("isRunning", true);
+        }
+        else if (JoystickManager.currentJoystick.Direction.sqrMagnitude >= 0.95 && stamina == 0)
+        {
+            resetNeeded = true;
+            isRunning = false;
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            isRunning = false;
+            animator.SetBool("isRunning", false);
+        }
+
+        if (memoryMovementX < 0)
+            GetComponent<SpriteRenderer>().flipX = true;
+        else
+           GetComponent<SpriteRenderer>().flipX = false;
+
+        if (timer >= 1)
+        {
+            if (isRunning)
+                stamina -= staminaLostOnRun;
+            if (!isRunning && stamina < maxStamina)
+                stamina += staminaGainedOnRest;
+            timer = 0;
+        }
 
         animator.SetFloat("Speed", JoystickManager.currentJoystick.Direction.sqrMagnitude);
-        animator.SetFloat("Horizontal", memoryMovementX);
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if (isRunning && !resetNeeded)
+        {
+            rb.MovePosition(rb.position + movement * runSpeed * Time.fixedDeltaTime);
+        }
+        else {
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+
+        if (stamina < maxStamina)
+        {
+            //stamina += 0.5f;
+        }
+            
+
     }
 }
