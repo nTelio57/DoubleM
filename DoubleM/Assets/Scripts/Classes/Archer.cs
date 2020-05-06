26fe8abe6519c6d5af17e81a388d243c3873fe19
Marius269 <61282240+Marius269@users.noreply.github.com>
2020-05-06 23:16:55 +0300

    Archer abilities done.
    https://doublemktu.atlassian.net/browse/DOUB-140

--- DoubleM/Assets/Scripts/Classes/Archer.cs
+++ DoubleM/Assets/Scripts/Classes/Archer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
     public class Archer : MonoBehaviour
     {
+        int critBoost = 0;
+        int dmgBoost = 0;
         public void AbilityPassive()
         {
 
@@ -18,8 +20,8 @@ using UnityEngine;
         IEnumerator AbilityOneEnum()
         {
 
-            float critChanceRoll = Random.value * 100;
-            float damage = BattleSystem.friendlyCurrentFighter.attackDamage;
+            float critChanceRoll = Random.value * 100 + critBoost;
+            float damage = BattleSystem.friendlyCurrentFighter.attackDamage + dmgBoost;
             if (critChanceRoll <= BattleSystem.friendlyCurrentFighter.critChance)
                 damage *= 1.5f;
 
@@ -30,6 +32,8 @@ using UnityEngine;
             yield return new WaitForSeconds(1);
             BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", false);
             yield return new WaitForSeconds(1);
+            critBoost = 0;
+            dmgBoost = 0;
         }
 
         public void AbilityTwo()
@@ -39,15 +43,36 @@ using UnityEngine;
 
         IEnumerator AbilityTwoEnum()
         {
-            Fighter f = BattleSystem.friendlyCurrentFighter;
-            int amount = (int)(0.3 * f.maxHP);
-            if (f.maxHP - f.currentHP < amount)
-                amount = f.maxHP - f.currentHP;
-            
-            setBattleText("<color=blue>Archer</color> restores <color=green>" + amount + "</color> HP", 1);
-            f.Heal(amount);
+            float critChanceRoll = Random.value * 100 + critBoost;
+            float damage = BattleSystem.friendlyCurrentFighter.attackDamage + dmgBoost;
+            Fighter[] eArray = FindObjectOfType<BattleSystem>().getAllFightersArray();
+            int[] te = getLowestHpEnemyIndex(eArray);
+            if (critChanceRoll <= BattleSystem.friendlyCurrentFighter.critChance)
+                damage *= 1.5f;
+            if(eArray.Length > 1)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    bool isDead = eArray[te[i]].TakeDamage((int)damage);
+                    setBattleText("<color=blue>Archer</color> hits for <color=red>" + (int)damage + "</color> damage", 1);
+                    Debug.Log(te[i] + " : " + eArray[te[i]].currentHP);
+                }
+            }
+            else
+            {
+                bool isDead = eArray[0].TakeDamage((int)damage);
+                setBattleText("<color=blue>Archer</color> hits for <color=red>" + (int)damage + "</color> damage", 1);
 
+                isDead = eArray[0].TakeDamage((int)damage);
+                setBattleText("<color=blue>Archer</color> hits for <color=red>" + (int)damage + "</color> damage", 1);
+            }
+            
+
+            BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", true);
             yield return new WaitForSeconds(1);
+            BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", false);
+            yield return new WaitForSeconds(1);
+
         }
 
         public void AbilityThree()
@@ -65,11 +90,12 @@ using UnityEngine;
                 bool isDead = eArray[i].TakeDamage((int)damage);
                 setBattleText("<color=blue>Archer</color> hits for <color=red>" + (int)damage + "</color> damage", 1);
 
-                BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", true);
-                yield return new WaitForSeconds(1);
-                BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", false);
-                yield return new WaitForSeconds(1);
+                
             }
+            BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", true);
+            yield return new WaitForSeconds(1);
+            BattleSystem.friendlyCurrentPrefab.GetComponent<Animator>().SetBool("IsAttacking", false);
+            yield return new WaitForSeconds(1);
         }
 
         public void AbilityFour()
@@ -80,8 +106,8 @@ using UnityEngine;
         IEnumerator AbilityFourEnum()
         {
             Fighter f = BattleSystem.friendlyCurrentFighter;
-            f.attackDamage = f.attackDamage + 2;
-            f.critChance = f.critChance + 2;
+            dmgBoost = dmgBoost + 2;
+            critBoost = critBoost + 2;
 
             setBattleText("<color=blue>Archer</color> enraged <color=red>+Crit +Dmg</color>", 1);
 
@@ -98,6 +124,37 @@ using UnityEngine;
         {
             BattleSystem bs = FindObjectOfType<BattleSystem>();
             bs.setBattleText(text, duration);
+        }
+
+        public int[] getLowestHpEnemyIndex(Fighter[] fighters)
+        {
+            int[] temp = new int[2];
+            int low1 = int.MaxValue;
+            int low2 = int.MaxValue;
+            int id1 = 0, id2 = 0;
+
+
+            for (int i = 0; i < fighters.Length; i++)
+            {
+                if (fighters[i].currentHP <= low1)
+                {
+                    low1 = fighters[i].currentHP;
+                    id1 = i;
+                }
+            }
+
+            for (int i = 0; i < fighters.Length; i++)
+            {
+                if (fighters[i].currentHP <= low2 && i != id1)
+                {
+                    low2 = fighters[i].currentHP;
+                    id2 = i;
+                }
+            }
+
+            temp[0] = id1;
+            temp[1] = id2;
+            return temp;
         }
     }
 
